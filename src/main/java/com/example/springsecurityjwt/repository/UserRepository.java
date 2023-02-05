c8fdf2dbabcee48e003b103e2a4946c2b0608e8c
Rufet Huseynov <103166770+ruhuseyn@users.noreply.github.com>
2023-02-05 21:58:32 +0400

    Some Changes

--- src/main/java/com/example/springsecurityjwt/repository/UserRepository.java
+++ src/main/java/com/example/springsecurityjwt/repository/UserRepository.java
@@ -2,6 +2,11 @@ package com.example.springsecurityjwt.repository;
 
 import com.example.springsecurityjwt.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
 }

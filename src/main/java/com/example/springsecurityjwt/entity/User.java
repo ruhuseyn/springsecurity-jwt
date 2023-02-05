0414a1f3ae3db2ac7c6830ffb5aac7efdf39d5a5
Rufet Huseynov <103166770+ruhuseyn@users.noreply.github.com>
2023-02-05 21:10:26 +0400

    Some Changes

--- src/main/java/com/example/springsecurityjwt/entity/User.java
+++ src/main/java/com/example/springsecurityjwt/entity/User.java
@@ -1,4 +1,26 @@
 package com.example.springsecurityjwt.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class User {
+
+    @Id
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
 }

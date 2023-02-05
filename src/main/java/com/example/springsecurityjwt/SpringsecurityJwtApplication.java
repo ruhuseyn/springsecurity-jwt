4c73966e96c9fd646dc12876821bb9fff179105a
Rufet Huseynov <103166770+ruhuseyn@users.noreply.github.com>
2023-02-05 23:08:59 +0400

    Some Changes

--- src/main/java/com/example/springsecurityjwt/SpringsecurityJwtApplication.java
+++ src/main/java/com/example/springsecurityjwt/SpringsecurityJwtApplication.java
@@ -1,10 +1,31 @@
 package com.example.springsecurityjwt;
 
+import com.example.springsecurityjwt.entity.User;
+import com.example.springsecurityjwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @SpringBootApplication
+@RequiredArgsConstructor
 public class SpringsecurityJwtApplication {
+
+    private final UserRepository userRepository;
+
+    public void createUsers() {
+        List<User> list = Stream.of(
+                new User(1, "rufet", "12345", "huseyn@gmail.com"),
+                new User(1, "alsu", "123456", "alsu@gmail.com"),
+                new User(1, "sebnem", "1234567", "sebnem@gmail.com"),
+                new User(1, "ugur", "12345678", "ugur@gmail.com")
+        ).collect(Collectors.toList());
+
+        userRepository.saveAll(list);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringsecurityJwtApplication.class, args);
---
+++ src/main/java/com/example/springsecurityjwt/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.springsecurityjwt.service;
+
+import com.example.springsecurityjwt.entity.User;
+import com.example.springsecurityjwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found : "+ username));
+        return new org.springframework.security.core.userdetails
+                .User(user.getUsername(),user.getPassword(),new ArrayList<>());
+    }
+}
